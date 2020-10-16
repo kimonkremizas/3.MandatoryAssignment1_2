2d8b69bf2cc51822a4fe0ca10d42391206b4f7ce
Kimon Kremyzas <kimo0016@edu.easj.dk>
2020-10-16 03:57:25 +0200

    completed

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,39 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+let inputElement:HTMLInputElement = <HTMLInputElement> document.getElementById("myInput")
+let buttonElement:HTMLButtonElement = <HTMLButtonElement> document.getElementById("myButton")
+let table:HTMLTableElement = <HTMLTableElement>document.getElementById("Table")
+let counter:number = 1;
+
+function ConvertToUpper(stringToConvert: string): string{
+    stringToConvert = stringToConvert.toUpperCase()
+    return stringToConvert
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function ConvertToLower(stringToConvert: string): string{
+    stringToConvert = stringToConvert.toLowerCase()
+    return stringToConvert
 }
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function manipulateAndAddString(){
+    let newRow:HTMLTableRowElement = <HTMLTableRowElement> table.insertRow(-1)
+    let id:HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(0)
+    let originalString: HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(1)
+    let manipulatedString: HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(2)
+    let conversion: HTMLTableCellElement = <HTMLTableCellElement> newRow.insertCell(3)
+    let inputText = (<HTMLInputElement>inputElement).value
+    let dropdown: HTMLSelectElement = <HTMLSelectElement> document.getElementById("dropdown")
+    let option: HTMLOptionElement = dropdown.options[dropdown.selectedIndex]
+    
+    id.innerHTML = counter.toString()
+    originalString.innerHTML = inputText
+    if (option.getAttribute("id") == "uppercase"){
+        manipulatedString.innerHTML = ConvertToUpper(inputText)
+        conversion.innerHTML = "!!! UPPERCASE !!!"
+    }
+    if (option.getAttribute("id") == "lowercase"){
+        manipulatedString.innerHTML = ConvertToLower(inputText)
+        conversion.innerHTML = "...lowercase..."
+    }    
+    counter++
+}
+
+document.getElementById("myButton").addEventListener("click", manipulateAndAddString)
